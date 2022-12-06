acda8a63e5388e068db2ca281bdef2bb9066d92c
Michalina Ignaś <m.ignas@becoolmen.com>
2022-12-06 10:20:34 +0100

    opis dzialan

--- opis_dzialan.cmd
+++ opis_dzialan.cmd
@@ -1,18 +1,18 @@
-#define ROTATE_SPEED     30
+#define ROTATE_SPEED     3
 /*
  *  Przykładowy zestaw poleceń
  */
- begin_parallel
-  Set   Podstawa 2 0 30    
-  Set   Podstawa.Ramie1.Ramie2 10 10 0   
-  end_parallel
-  begin_parallel
-  Rotate Podstawa.Ramie1 ROTATE_SPEED 40
-  end_parallel
+ // begin_parallel
+  // Set   Podstawa 2 0 30    
+  // Set   Podstawa.Ramie1.Ramie2 10 10 0   
+  // end_parallel
+  // begin_parallel
+  // Rotate Podstawa.Ramie1 ROTATE_SPEED 40
+  // end_parallel
   Pause 1000 
-  Move  Podstawa.Ramie1  10 10
+  Move  Podstawa.Ramie1  1 10
   begin_parallel
   Rotate Podstawa.Ramie1 ROTATE_SPEED 60 
-  Move  Podstawa.Ramie1.Ramie2 10 20  
+  Move  Podstawa.Ramie1.Ramie2 1 20  
   end_parallel         
   
